337aeb2c175e87381ef1150980c399b8f0f0621f
thirupurtamil <thirupurtamil@gmail.com>
2023-12-26 08:05:20 +0530

    2.1

--- tselvi/papa/urls.py
+++ tselvi/papa/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include 
-from papa import views 
+from papa import views
+
 
 urlpatterns = [
     path('selvi', admin.site.urls),
