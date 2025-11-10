b024ffd7cc9b72cba44ed08ce13e4c5bc5f9c72e
thirupurtamil <thirupurtamil@gmail.com>
2025-11-10 18:22:14 +0530

    4

--- nse/settings.py
+++ nse/settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',"ss",
-    'rest_framework','ts',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- nse/urls.py
+++ nse/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("selvi/", admin.site.urls),
-    path('', include('ts.urls')),
     path('', include('ss.urls'))
     
     
